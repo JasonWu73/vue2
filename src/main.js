a4ef3f23654d383b6cae12549a25671702cc82ef
wuxianjie <wuxianjiezh@gmail.com>
2021-07-26 15:10:06 +0800

    Vue2 项目环境

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api'
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
